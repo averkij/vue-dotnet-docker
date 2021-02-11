b0461413f926dd7d4768dd86b31cdf3b42fde323
Sergei Averkiev <averoO@gmail.com>
2021-02-11 17:28:55 +0300

    Add description.

--- fe/src/App.vue
+++ fe/src/App.vue
@@ -28,11 +28,11 @@
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="https://github.com/averkij/vue-dotnet-docker"
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
+        <span class="mr-2">Source</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
--- fe/src/components/HelloWorld.vue
+++ fe/src/components/HelloWorld.vue
@@ -2,90 +2,68 @@
   <v-container>
     <v-row class="text-center">
       <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
+        <v-img :src="require('../assets/logo.svg')" class="my-3" contain height="200" />
       </v-col>
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
+          Web app boilerplate
         </h1>
 
         <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
+          Backend and frontend tiers backed by Docker.
         </p>
       </v-col>
 
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
+      <v-col class="mb-5" cols="12">
         <h2 class="headline font-weight-bold mb-3">
-          What's next?
+          Backend
         </h2>
 
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
+        <p class="subheading font-weight-regular">
+          .NET 5 application
+        </p>
+        <p class="subheading font-weight-regular">
+          Backend app have one test API handler which returns random number.
+        </p>
+        <v-btn class="mt-5" @click="getNewNumber()">Get number</v-btn>
+        <p v-show="numberFromBackend" class="text-h4 font-weight-bold mt-5">
+          {{numberFromBackend}}
+        </p>
+      </v-col>
+
+      <v-col class="mb-5 text-center" cols="12">
+        <h2 class="headline font-weight-bold mb-3">
+          Frontend
+        </h2>
+
+        <p class="subheading font-weight-regular">
+          Web application made with Vue.js
+        </p>
+        <p class="subheading font-weight-regular">
+          Necessary packages are already configured. See the usage in code.
+        </p>
+        <v-row class="d-flex justify-center mt-5">
+          <ul style="width:300px;">
+            <li class="text-left"><b>vuex</b> for managing state</li>
+            <li class="text-left"><b>vue-router</b> for routing</li>
+            <li class="text-left"><b>vuetify</b> for the fast and neat UI</li>
+          </ul>
         </v-row>
       </v-col>
 
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
+      <v-col class="mb-5" cols="12">
         <h2 class="headline font-weight-bold mb-3">
-          Important Links
+          Docker
         </h2>
 
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
+        <p class="subheading font-weight-regular">
+          Deploy the app with a few commands.
+        </p>
 
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
+        <p class="subheading font-weight-regular">
+          <b>docker-compose.yaml</b> contains the deployment configuration.
+        </p>
       </v-col>
     </v-row>
   </v-container>
@@ -96,56 +74,12 @@
     name: 'HelloWorld',
 
     data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com',
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer',
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
+      numberFromBackend: null
     }),
+    methods: {
+      getNewNumber() {
+        this.numberFromBackend = 42;
+      }
+    }
   }
 </script>
