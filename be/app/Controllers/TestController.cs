57b6f9cb0d8ea7664dbc25a21b13db317d40d5ad
Sergei Averkiev <averoO@gmail.com>
2021-02-11 10:45:06 +0300

    Init BE + FE with vuetify.

---
+++ be/app/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace lingtrain_web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public int Get()
+        {
+            var rnd = new Random();
+
+            return rnd.Next(1,100);
+        }
+    }
+}
